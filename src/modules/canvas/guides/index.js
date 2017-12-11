8c5a96d1e6d5b702f9b489af1e9b92703c05e492
Matt Kaemmerer <matthew.kaemmerer@gmail.com>
2017-12-11 14:05:09 +0900

    Add ticks to rotation guides

--- src/modules/canvas/guides/index.js
+++ src/modules/canvas/guides/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { V2 } from 'utils/vectors';
+import { range } from 'utils/generators/fluent';
 
 import styles from './index.scss';
 import classnames from 'classnames/bind';
@@ -33,6 +34,24 @@ const arcString = (arc) => {
   `;
 }
 
+const RotationTicks = () => {
+  const ticks = range(0, 24)
+    .map((x) => {
+      const transform = `rotate(${x * 360/24})translate(35,0)`;
+      const rx = x % 6 === 0
+        ? '3'
+        : '1.5';
+      const ry = '1';
+
+      return (
+        <ellipse key={x} transform={transform} rx={rx} ry={ry}/>
+      );
+    })
+    .toArray();
+
+  return (<g className={cx('guide_ticks')}>{ticks}</g>);
+};
+
 const Guides = ({ placement, movement, rotation, showMovement, showRotation }) => {
   const moveGuideClasses = cx(
     'guide',
@@ -60,6 +79,7 @@ const Guides = ({ placement, movement, rotation, showMovement, showRotation }) =
         <circle className={cx('guide_circle')} r="30"/>
         <path className={cx('guide_arc')} d={arc}/>
         <line className={cx('guide_line')} transform={toRotation(rotation)} x1="0" y1="0" x2="1000" y2="0"/>
+        <RotationTicks/>
       </g>
     </g>
   );
