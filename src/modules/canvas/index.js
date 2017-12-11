0045d3b837ec995f8c5716319fe50a34a88d5c4d
Matt Kaemmerer <matthew.kaemmerer@gmail.com>
2017-12-11 12:34:18 +0900

    Move canvas modules under canvas directory

--- src/modules/canvas/index.js
+++ src/modules/canvas/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Turtle from '../turtle';
-import Drawing from '../drawing';
+import Turtle from './turtle';
+import Drawing from './drawing';
 
 import styles from './index.scss';
 import classnames from 'classnames/bind';
