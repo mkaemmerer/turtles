c439202ffef67cb4906075aab72b9cc89f966ecb
Matt Kaemmerer <matthew.kaemmerer@gmail.com>
2018-05-21 20:56:58 +0900

    Move app stuff up one level

--- src/app.js
+++ src/app.js
@@ -1,4 +1,13 @@
+import React from 'react';
+import program from '../programs/square-vars.tt';
 import TurtleApp from 'modules/turtle-app';
 import './style/document.scss';
 
-export default TurtleApp;
+const App = (props) => (
+  <TurtleApp
+    {...props}
+    initialProgram={program}
+  />
+);
+
+export default App;
