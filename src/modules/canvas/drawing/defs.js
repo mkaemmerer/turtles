47bba55d255c474485c4ee19d7a8769260403186
Matt Kaemmerer <matthew.kaemmerer@gmail.com>
2018-05-29 16:36:46 +0900

    Use different width arrow head for move vs turn

--- src/modules/canvas/drawing/defs.js
+++ src/modules/canvas/drawing/defs.js
@@ -8,15 +8,27 @@ const Markers = () => (
   <React.Fragment>
     <marker
       className={cx('mark_arrowhead')}
-      id="arrowhead"
+      id="arrowhead-move"
       viewBox="0 0 10 10"
-      refX="10"
+      refX="9"
       refY="5"
       markerWidth="4"
       markerHeight="4"
       orient="auto"
     >
       <path d="M 0 0 L 10 5 L 0 10 z" />
+    </marker>
+    <marker
+      className={cx('mark_arrowhead')}
+      id="arrowhead-turn"
+      viewBox="0 0 10 10"
+      refX="6"
+      refY="3"
+      markerWidth="4"
+      markerHeight="4"
+      orient="auto"
+    >
+      <path d="M 0 0 L 10 3 L 0 6 z" />
     </marker>
   </React.Fragment>
 );
